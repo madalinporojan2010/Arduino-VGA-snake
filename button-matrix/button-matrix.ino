ec63cc34a8b7c16a139c4ce2d1c5ed35ace405c9
Madalin Marian Porojan <madalin.porojan2000@gmail.com>
2022-11-12 09:53:54 +0200

    Update button-matrix.ino

--- button-matrix/button-matrix.ino
+++ button-matrix/button-matrix.ino
@@ -1,44 +1,56 @@
-#include <Keypad.h>
-
-
 #define ledpin 13
 
+#define R4_PIN 48
 
-const byte rows = 3;
-const byte cols = 2;
-char keys[rows][cols] = {
-  {'*','>'},
-  {'^','_'},
-  {'#','<'} 
-};
+#define C1_PIN 47
+#define C2_PIN 46
+#define C3_PIN 45
+#define C4_PIN 44
 
-byte rowPins[rows] = {48, 47, 46}; // matrix pins
-byte colPins[cols] = {44, 45};
+#define R1_MOVEMENT_PIN R4_PIN
 
-Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, rows, cols);
+#define C1_MOVEMENT_PIN C4_PIN
+#define C2_MOVEMENT_PIN C3_PIN
+#define C3_MOVEMENT_PIN C2_PIN
+#define C4_MOVEMENT_PIN C1_PIN
 
-void setup() { 
-  pinMode(ledpin,OUTPUT);
-  digitalWrite(ledpin, HIGH);
-  Serial.begin(9600); 
+unsigned int UP, DOWN, LEFT, RIGHT;
+char key;
+
+void setup() {
+  pinMode(R1_MOVEMENT_PIN, OUTPUT);
+  pinMode(R1_MOVEMENT_PIN, HIGH);
+
+  pinMode(C1_MOVEMENT_PIN, INPUT_PULLUP);
+  pinMode(C2_MOVEMENT_PIN, INPUT_PULLUP);
+  pinMode(C3_MOVEMENT_PIN, INPUT_PULLUP);
+  pinMode(C4_MOVEMENT_PIN, INPUT_PULLUP);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  char key = keypad.getKey();
-    if(key) 
-    {
-      switch (key)
-      {
-        case '*':
-          digitalWrite(ledpin, LOW);
-          Serial.println(key);
-          break;
-        case '#':
-          digitalWrite(ledpin, HIGH);
-          Serial.println(key);
-          break;
-        default:
-          Serial.println(key);
-      }
-    }
+  key = readKeys(C1_MOVEMENT_PIN, C2_MOVEMENT_PIN, C3_MOVEMENT_PIN, C4_MOVEMENT_PIN);
+  if (key) {
+    Serial.println(key);
+  }
+}
+
+char readKeys(byte C1, byte C2, byte C3, byte C4) {
+  UP = digitalRead(C1);
+  DOWN = digitalRead(C2);
+  LEFT = digitalRead(C3);
+  RIGHT = digitalRead(C4);
+
+  if (UP == 0) {
+    return '^';
+  } else if (DOWN == 0) {
+    return '_';
+  } else if (LEFT == 0) {
+    return '<';
+  } else if (RIGHT == 0) {
+    return '>';
+  } else {
+    return NULL;
+  }
 }
