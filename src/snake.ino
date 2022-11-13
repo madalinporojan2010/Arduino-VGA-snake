d0aa6d8b7f87d4eee977bee40938ba5af865f0f1
Madalin Marian Porojan <madalin.porojan2000@gmail.com>
2022-11-13 23:42:02 +0200

    Update snake.ino

--- src/snake.ino
+++ src/snake.ino
@@ -454,8 +454,8 @@ void drawScoreText() {
 
 void drawGameOverText() {
     if(gameOver) {
-        vga.printPROGMEM((byte*)fnt_ufont_data, FNT_UFONT_SYMBOLS_COUNT, FNT_UFONT_HEIGHT, 3, 1, gameOver_Message0, VGAX_WIDTH / 2 - 23, 30, BLUE);
-        vga.printPROGMEM((byte*)fnt_ufont_data, FNT_UFONT_SYMBOLS_COUNT, FNT_UFONT_HEIGHT, 3, 1, gameOver_Message1, VGAX_WIDTH / 2 - 30, 37, BLUE);
+        vga.printPROGMEM((byte*)fnt_ufont_data, FNT_UFONT_SYMBOLS_COUNT, FNT_UFONT_HEIGHT, 3, 1, gameOver_Message0, VGAX_WIDTH / 2 - 23, 30, BLACK);
+        vga.printPROGMEM((byte*)fnt_ufont_data, FNT_UFONT_SYMBOLS_COUNT, FNT_UFONT_HEIGHT, 3, 1, gameOver_Message1, VGAX_WIDTH / 2 - 30, 37, BLACK);
     }
 }
 
